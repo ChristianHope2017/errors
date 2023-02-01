5320c268c36e7925af1fbf19429472641f02f825
Christian Hope <2017117171@ub.edu.bz>
2023-02-01 13:04:48 -0600

    added error checking to area

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 // Demonstrate errors
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
-func area(length float64, width float64)float64{
-	result := length * width
-	return result
+func area(lenght float64, width float64)(float64, error){
+	//check if length is greater than 0
+	if lenght < 0{
+		err := errors.New("you cannot have a negative lenght")
+		return -1,err
+	}
+
+	if width < 0{
+		err := errors.New("you cannot have a negative width")
+		return -1,err
+	}
+	result := lenght * width
+	return result, nil
 }
 
 func main(){
-	length := 5.5
-	width := -10
-	result := area(length, float64(width))
+	lenght := 5.5
+	width := 10
+	result, err := area(lenght, float64(width))
+	if err != nil{
+		log.Fatal(err)
+	}
 	fmt.Println(result)
 }
